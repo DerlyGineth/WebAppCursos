6c8dfcb121b058a9d802536b74538877b11ce99e
DerlyGineth <54144244+DerlyGineth@users.noreply.github.com>
2019-09-12 13:09:01 -0500

    Proyecto Cursos 1.0
    proyecto completo

---
+++ WebAppCursos/Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCursos.Models
+{
+    public class Categoria
+    {
+
+
+        public int CategoriaID { get; set;}
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public Boolean Estado { get; set; } = true;
+
+        public ICollection<Curso> Cursos { get; set; }
+
+    }
+}
